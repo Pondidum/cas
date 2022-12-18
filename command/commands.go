c21d7d5fb913ef747829bb6abffedb0b77b6ed8d
andy <andy@sammalmaa.fi>
2022-12-18 14:44:59 +0200

    commands: cleanup

--- command/commands.go
+++ command/commands.go
@@ -28,44 +28,5 @@ func Commands(ui cli.Ui) map[string]cli.CommandFactory {
 		"hash": func() (cli.Command, error) {
 			return NewHashCommand(ui), nil
 		},
-
-		// "write": func() (cli.Command, error) {
-		// 	cmd := &WriteCommand{}
-		// 	cmd.Meta = NewMeta(ui, cmd)
-
-		// 	return cmd, nil
-		// },
-
-		// "read": func() (cli.Command, error) {
-		// 	cmd := &ReadCommand{}
-		// 	cmd.Meta = NewMeta(ui, cmd)
-
-		// 	return cmd, nil
-		// },
-
-		// "store": func() (cli.Command, error) {
-		// 	cmd := &StoreCommand{}
-		// 	cmd.Meta = NewMeta(ui, cmd)
-
-		// 	return cmd, nil
-		// },
-
-		// "fetch": func() (cli.Command, error) {
-		// 	cmd := &FetchCommand{}
-		// 	cmd.Meta = NewMeta(ui, cmd)
-
-		// 	return cmd, nil
-		// },
-
-		// "hash": func() (cli.Command, error) {
-		// 	pwd, err := os.Getwd()
-		// 	if err != nil {
-		// 		return nil, err
-		// 	}
-		// 	cmd := &HashCommand{fs: os.DirFS(pwd)}
-		// 	cmd.Meta = NewMeta(ui, cmd)
-
-		// 	return cmd, nil
-		// },
 	}
 }
