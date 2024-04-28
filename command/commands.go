68bfde5feafe3a2824d9bd46aebaa6ff491366df
andy <andy@sammalmaa.fi>
2024-04-28 19:36:08 +0300

    version: refactor to new command style

--- command/commands.go
+++ command/commands.go
@@ -11,13 +11,7 @@ func Commands(ui cli.Ui) map[string]cli.CommandFactory {
 	storage := &localstorage.FileStore{}
 
 	return map[string]cli.CommandFactory{
-		"version": func() (cli.Command, error) {
-			cmd := &VersionCommand{}
-			cmd.Meta = NewMeta(ui, cmd)
-
-			return cmd, nil
-		},
-
+		"version":  NewCommand("version", NewVersionCommand()),
 		"fetch":    NewCommand("fetch", NewFetchCommand(storage)),
 		"artifact": NewCommand("artifact", NewArtifactCommand(storage)),
 
--- command/version.go
+++ command/version.go
@@ -1,17 +1,26 @@
 package command
 
 import (
+	"cas/config"
 	"cas/version"
 	"context"
 	"fmt"
 	"strings"
 
 	"github.com/charmbracelet/glamour"
-	"github.com/spf13/pflag"
 )
 
+func NewVersionCommand() *VersionCommand {
+	cmd := &VersionCommand{}
+
+	cmd.cfg = append(cmd.cfg, cmd.commandFlags())
+	cmd.cfg = append(cmd.cfg, globalFlags())
+
+	return cmd
+}
+
 type VersionCommand struct {
-	Meta
+	cfg []*config.ConfigGroup
 
 	printLog bool
 	rawLog   bool
@@ -26,17 +35,25 @@ func (c *VersionCommand) Synopsis() string {
 	return "Prints the version number"
 }
 
-func (c *VersionCommand) Flags() *pflag.FlagSet {
-	flags := pflag.NewFlagSet(c.Name(), pflag.ContinueOnError)
-	flags.BoolVar(&c.printLog, "changelog", false, "print the changelog")
-	flags.BoolVar(&c.short, "short", false, "show only the version, not the sha")
-	flags.BoolVar(&c.rawLog, "raw", false, "print the plain markdown from the changelog")
-
-	return flags
+func (c *VersionCommand) Usages() []string {
+	return []string{
+		`cas artifact "${hash}" ./path/to/artifact`,
+		`cas artifact "$<" "$@"`,
+	}
 }
 
-func (c *VersionCommand) EnvironmentVariables() map[string]string {
-	return map[string]string{}
+func (c *VersionCommand) commandFlags() *config.ConfigGroup {
+	cfg := config.NewConfigGroup("")
+
+	cfg.BoolFlag(&c.printLog, "changelog", "", false, "print the changelog")
+	cfg.BoolFlag(&c.short, "short", "", false, "show only the version, not the sha")
+	cfg.BoolFlag(&c.rawLog, "raw", "", false, "print the plain markdown from the changelog")
+
+	return cfg
+}
+
+func (c *VersionCommand) Configuration() []*config.ConfigGroup {
+	return c.cfg
 }
 
 func (c *VersionCommand) RunContext(ctx context.Context, args []string) error {
@@ -44,9 +61,9 @@ func (c *VersionCommand) RunContext(ctx context.Context, args []string) error {
 	change := version.Changelog()
 
 	if c.short {
-		c.Ui.Output(change[0].Version)
+		fmt.Println(change[0].Version)
 	} else {
-		c.Ui.Output(fmt.Sprintf(
+		fmt.Println(fmt.Sprintf(
 			"%s - %s",
 			change[0].Version,
 			version.VersionNumber(),
@@ -55,12 +72,11 @@ func (c *VersionCommand) RunContext(ctx context.Context, args []string) error {
 
 	if c.printLog {
 		if c.rawLog {
-			c.Ui.Output(strings.TrimSpace(change[0].Log))
+			fmt.Println(strings.TrimSpace(change[0].Log))
 		} else {
 			out, _ := glamour.Render(change[0].Log, "dark")
-			c.Ui.Output(strings.TrimSpace(out))
+			fmt.Println(strings.TrimSpace(out))
 		}
-
 	}
 
 	return nil
